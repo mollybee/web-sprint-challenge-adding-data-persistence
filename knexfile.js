5a9c453cc702c526a5eb3d0fe842d5fa26697c80
Molly Brown <mollybrown0516@gmail.com>
2021-06-25 11:16:50 -0400

    Completed installing dependencies and setting up my environment and files.

--- knexfile.js
+++ knexfile.js
@@ -1,20 +1,72 @@
-// do not make changes to this file
+// Update with your config settings.
 const sharedConfig = {
   client: 'sqlite3',
   useNullAsDefault: true,
-  migrations: { directory: './data/migrations' },
-  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+  migrations: {
+    directory: './data/migrations',
+  },
+  // seeds: {
+  //   directory: './data/seeds',   //////THIS IS OPTIONAL
+  // },
+  // this enables foreign keys in SQLite
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done)
+    },
+  },
 }
 
+
+
+
 module.exports = {
+
+  // development: {
+  //   client: 'sqlite3',
+  //   connection: {
+  //     filename: './dev.sqlite3'
+  //   }
+  // },
+
   development: {
     ...sharedConfig,
-    connection: { filename: './data/lambda.db3' },
-    seeds: { directory: './data/seeds' },
+    connection: { filename: './dev.sqlite3' },
   },
-  testing: {
-    ...sharedConfig,
-    connection: { filename: './data/test.db3' },
-  },
-};
 
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+
+
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
